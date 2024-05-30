23e1b0bcf8082638d5cbeb5596d720953a54e1fc
4sh1s <tripathy.ashish251997@gmail.com>
2024-05-30 21:58:09 +0530

    todo list with ai functionality is up

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <>
       <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-gray-800 border-b border-gray-200 text-sm py-3 sm:py-0 ">
         <nav
           className="relative max-w-7xl w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
@@ -17,6 +16,5 @@ export default function Header() {
           </div>
         </nav>
       </header>
-    </>
   );
 }
